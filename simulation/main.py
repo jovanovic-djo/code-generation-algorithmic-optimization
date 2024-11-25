26a08a1e981c63bacbfd8920fde01bb8339292ce
Djordje J <jovanovic.djo@gmail.com>
2024-11-25 17:08:05 +0100

    add initial zigzag and spiral solver

--- simulation/main.py
+++ simulation/main.py
@@ -1,5 +1,17 @@
 from src.game import SnakeGame
+# from solvers.zigzag_solver import ZigzagSolver
+# from solvers.spiral_solver import SpiralSolver
+from solvers.base_solver import BaseSolver
+import threading
+
+def run_solver(solver_class):
+    game = SnakeGame()
+    solver = solver_class(game)
+    
+    solver_thread = threading.Thread(target=solver.solve)
+    solver_thread.start()
+    
+    game.run()
 
 if __name__ == "__main__":
-    game = SnakeGame()
-    game.run()
+    run_solver(BaseSolver)
